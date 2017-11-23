e9a10d8040fcbc5529adbd8ee293ca32b6c2a52b
Shouhai Liu <shouhai.liu@ebcont.com>
2017-11-23 16:30:45 +0100

    JAMP-5963: added WRWKS CI infrastructure

---
+++ sidemenu-addon/jenkinsfile.groovy
@@ -0,0 +1,29 @@
+@Library('pipe.commons.jenkinsfile@master') _
+
+pipeline {
+
+    agent any
+
+    tools {
+        maven "Maven 3.3.3"
+        jdk "Java 1.8.0"
+    }
+
+    stages {
+        stage('test') {
+            steps {
+                script {
+                    sh "mvn clean deploy -Dmaven.test.failure.ignore=true -e -B"
+                    junit "**/target/surefire-reports/**/*.xml"
+                }
+            }
+        }
+    }
+
+    post {
+        changed { script { sendChangedNotification currentBuild.result } }
+        failure { script { sendFailureNotification currentBuild.result } }
+        unstable { script { sendFailureNotification currentBuild.result } }
+        aborted { script { sendFailureNotification currentBuild.result } }
+    }
+}
